d289a6d9c0f81c402cb7cb3f1fcfba3e90d1a8cd
takushi <sokoninoruna@gmail.com>
2020-10-10 02:07:31 +0900

    ランダムに数値が表示されるよう変更　スタートボタン押下時にルーレットの処理追加　同値なら色変化

--- JavaScript/Bingo/index.js
+++ JavaScript/Bingo/index.js
@@ -1,21 +1,39 @@
 var parentSquare = document.getElementById('parentSquare');
 var rouletteStart = document.getElementById('rouletteStart');
 var message = document.getElementById('message');
+var messages = [];
 
-numbers = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]];
+var count = 1;
+while (count <= 24) {
+    var num01 = Math.round(Math.random() * 4);
+    var num02 = Math.round(Math.random() * 4);
+    if (parentSquare.children.item(num01).children.item(num02).textContent.length === 0) {
+        insertNumber(num01, num02, count);
+        count++;
+    }
+}
+
+function insertNumber(num01, num02, count) {
+    parentSquare.children.item(num01).children.item(num02).textContent = count;
+}
 
 rouletteStart.addEventListener('click', function () {
-    message.innerHTML = "";
-    var num01 = Math.floor(Math.random() * 4);
-    var num02 = Math.floor(Math.random() * 4);
-    if (parentSquare.children.item(num01).children.item(num02).textContent.length === 0) {
-        insertNumber(num01, num02);
-        console.log(insertNumber(num01, num02));
-    } else {
-        message.innerHTML = "数値がかぶったのでもう一度やり直し。";
+    var countUp = 0;
+    while (countUp === 0) {
+        var num = Math.round(Math.random() * 23) + 1;
+        messages.push(num);
+        message.innerHTML = messages;
+        changeColor(messages);
+        countUp++;
     }
 });
 
-var insertNumber = function (num01, num02) {
-    return parentSquare.children.item(num01).children.item(num02).textContent = numbers[num01][num02];
+function changeColor(messages) {
+    for (var x = 0; x <= 4; x++) {
+        for (var y = 0; y <= 4; y++) {
+            if (messages.includes(parseInt(parentSquare.children.item(x).children.item(y).textContent))) {
+                parentSquare.children.item(x).children.item(y).style.color = 'red';
+            }
+        }
+    }
 }
