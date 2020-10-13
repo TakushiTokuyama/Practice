10331e6cd96af7a63761d3168820f640af5fc266
takushi <sokoninoruna@gmail.com>
2020-10-13 23:29:49 +0900

    ビンゴ時のパターン　追加

--- JavaScript/Bingo/index.js
+++ JavaScript/Bingo/index.js
@@ -4,38 +4,50 @@ var roulettReset = document.getElementById('roulettReset');
 var rouletteResult = document.getElementById('rouletteResult');
 var message = document.getElementById('message');
 var numbers = [];
+var checkNumbers = [[], [], [], [], []];
+var checker = 0;
+var insertNums = [];
 
-var count = 1;
+var count = 0;
 
 // 初期表示
 window.onload = function () {
     InsertRandomNumber();
+    // 真ん中はFREEマス
+    checkNumbers[2][2] = 1;
 };
 
-// ビンゴカードに数値をランダムに配置
+// ビンゴカードに数値1-50をランダムに配置
 function InsertRandomNumber() {
-    while (count <= 24) {
+    while (count < 24) {
         var num01 = Math.round(Math.random() * 4);
         var num02 = Math.round(Math.random() * 4);
         if (parentSquare.children.item(num01).children.item(num02).textContent.length === 0) {
-            insertNumber(num01, num02, count);
-            count++;
+            if (insertNumber(num01, num02, count)) {
+                count++;
+            }
         }
     }
 }
 
-// 数値をマスに追加
+// 数値1-50をマスに追加
 function insertNumber(num01, num02, count) {
-    parentSquare.children.item(num01).children.item(num02).textContent = count;
+    var randomNum = Math.round(Math.random() * 49) + 1;
+    if (!insertNums.includes(randomNum)) {
+        insertNums[count] = randomNum;
+        parentSquare.children.item(num01).children.item(num02).textContent = randomNum;
+        return true;
+    }
+    return false;
 }
 
-// ルーレット
+// ルーレット 25でstop
 rouletteStart.addEventListener('click', function () {
     var countUp = 0;
     while (countUp === 0) {
-        var num = Math.round(Math.random() * 23) + 1;
+        var num = Math.round(Math.random() * 49) + 1;
         if (numbers.includes(num)) {
-            if (numbers.length >= 24) {
+            if (numbers.length >= 25) {
                 break;
             }
             continue;
@@ -54,6 +66,7 @@ function changeColor(numbers) {
         for (var y = 0; y <= 4; y++) {
             if (numbers.includes(parseInt(parentSquare.children.item(x).children.item(y).textContent))) {
                 parentSquare.children.item(x).children.item(y).style.color = 'red';
+                checkBingo(x, y);
             }
         }
     }
@@ -63,3 +76,58 @@ function changeColor(numbers) {
 roulettReset.addEventListener('click', function () {
     location.reload();
 });
+
+// bingoかどうかを調べる
+function checkBingo(x, y) {
+    checkNumbers[x][y] = 1;
+    checkSide();
+    checkVertical();
+    checkDiagonal();
+}
+
+// 横列
+function checkSide() {
+    for (var i = 0; i < 5; i++) {
+        for (var n = 0; n < 5; n++) {
+            checker += checkNumbers[i][n];
+            resultProcess(checker);
+        }
+        checker = 0;
+    }
+    checker = 0;
+}
+
+// 縦列
+function checkVertical() {
+    for (var i = 0; i < 5; i++) {
+        for (var n = 0; n < 5; n++) {
+            checker += checkNumbers[n][i];
+            resultProcess(checker);
+        }
+        checker = 0;
+    }
+    checker = 0;
+}
+
+// 斜め
+function checkDiagonal() {
+    for (var i = 0; i < 5; i++) {
+        checker += checkNumbers[i][i];
+    }
+    resultProcess(checker);
+    checker = 0;
+    for (var i = 0; i < 5; i++) {
+        checker += checkNumbers[i][4 - i];
+    }
+    resultProcess(checker);
+    checker = 0;
+}
+
+// 結果処理
+function resultProcess(checker) {
+    if (checker === 5) {
+        message.innerHTML = 'BINGO!!';
+        rouletteStart.setAttribute('disabled', true);
+        rouletteStart.innerHTML = 'BINGO!!';
+    }
+}
