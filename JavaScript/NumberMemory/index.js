30998d6a8a3d0b9af9ab076fac56c2db303aa538
takushi <sokoninoruna@gmail.com>
2020-10-30 23:56:20 +0900

    一旦push

---
+++ JavaScript/NumberMemory/index.js
@@ -0,0 +1,27 @@
+var countDown = document.getElementById('countDown');
+var randomNumber = document.getElementById('randomNumber');
+var count = 3;
+var timer;
+
+// 1000ミリ秒ごとに繰り返す
+var countDownTimer = () => {
+    timer = setInterval(function () { NumberLow() }, 2000);
+}
+
+// countを1ずつ減らして表示
+function NumberLow() {
+    countDown.innerHTML = count;
+    --count;
+    if (count < -1) {
+        clearInterval(timer);
+        countDown.innerHTML = "";
+        addRandomNumber();
+    }
+}
+
+// 数値をrandom表示
+var addRandomNumber = () => {
+    randomNumber.innerHTML = Math.ceil(Math.random() * 99);
+}
+
+countDownTimer();
