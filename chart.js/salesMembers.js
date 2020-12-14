367822e84a39963f9ef1b329dd5d26cce839e27a
TakushiTokuyama <55052628+TakushiTokuyama@users.noreply.github.com>
2020-12-14 23:48:36 +0900

    chart.js sample

---
+++ chart.js/salesMembers.js
@@ -0,0 +1,9 @@
+export const salesMembers = [
+    {name: "Json", salesHistory: 5, expertArea01:'金融', expertArea02:'通信', expertArea03:'飲食', visitNum:4},
+    {name: "Batch", salesHistory: 22, expertArea01:'通信', expertArea02:'物流', expertArea03:'小売', visitNum:3},
+    {name: "Kevin", salesHistory: 3, expertArea01:'金融', expertArea02:'物流', expertArea03:'通信', visitNum:8},
+    {name: "Rich", salesHistory: 8, expertArea01:'飲食', expertArea02:'通信', expertArea03:'物流', visitNum:2},
+    {name: "Bega", salesHistory: 14, expertArea01:'金融', expertArea02:'通信', expertArea03:'物流', visitNum:11}
+];
+
+export const experts = { 0:'金融', 1:'物流', 2:'通信', 3:'飲食', 4:'小売'};
