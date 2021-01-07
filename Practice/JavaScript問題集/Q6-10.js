309afd77ced4a1a666ea9ac7e66feaed65f80cf1
takushi <sokoninoruna@gmail.com>
2021-01-07 23:46:08 +0900

    forEach

---
+++ Practice/JavaScript問題集/Q6-10.js
@@ -0,0 +1,35 @@
+// Q.6
+// if (typeof x === 'undefined') {
+
+// }
+// typeof x の型はundefined
+
+// if (x === undefined) {
+
+// }
+// xは存在しないためerror
+
+// Q.7
+// let x;
+// if (x === void 0) {
+
+// }
+
+// void 0でundefinedとなる。
+// void (0);
+// void "別の書き方";
+
+// if (y === void 0) {
+
+// }
+// yを宣言していないため、こちらはエラー
+
+// Q.8
+// const obj = {
+//     key: 'aa',
+//     key2: 'bb'
+// }
+
+// for (const [key, value] of Object.entries(obj)) {
+//     console.log(`${key}`, `${value}`);
+// }
