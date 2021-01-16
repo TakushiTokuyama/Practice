3600879e90ed02811ccf7245cc4c635a14b435ee
takushi <sokoninoruna@gmail.com>
2021-01-16 18:47:17 +0900

    Q12-15追加

--- Practice/JavaScript問題集/Q11-15.js
+++ Practice/JavaScript問題集/Q11-15.js
@@ -1,3 +1,4 @@
+'use strict'
 // Q.11
 // let arry = [{ id: 1, name: 'morita' }, { id: 2, name: 'kenji' }, { id: 4, name: 'uro' }, { id: 3, name: 'ken' }];
 
@@ -18,3 +19,99 @@
 // });
 
 // console.log(sortByArry);
+
+// Q.12
+// function def(a = 5, b = 7) {
+//     return a + b;
+// }
+
+// console.log(def(1));
+
+// function def(a = 5, b = 0) {
+//     console.log(typeof b);
+//     return a + b;
+// }
+
+// console.log(def());
+
+// const array = [a = 5, b = 7];
+
+// console.log(array[0] = 1, array[1]);
+
+// Q.13
+// var count = 0;
+// function next() {
+//     return ++count;
+// }
+
+// const setUp = function () {
+//     let count = 0;
+//     return ++count;
+// };
+
+// const setUp = function () {
+//     let count = 0;
+//     return function () {
+//         return ++count;
+//     }
+// };
+
+// const setUp = function () {
+//     let count = 0;
+//     return function () {
+//         return function () {
+//             return ++count;
+//         }
+//     }
+// };
+
+// console.log(setUp());
+
+// const next = setUp();
+// const next1 = next();
+
+//  console.log(next());
+//  console.log(next());
+
+// console.log(next1());
+// console.log(next1());
+
+// Q.14
+
+// function fun(a, b, c) {
+//     var array = [a, b, c]
+//     return array;
+// }
+
+// console.log(fun(1, 2, 3));
+
+// function fun() {
+//     console.log(arguments);
+//     return Array.from(arguments);
+// }
+
+// console.log(Array.from('abc'));
+
+// Array.from()
+// 配列のようなObjectもしくは反復可能Objectから新しいArrayインスタンスを生成
+
+// console.log(fun(1, 2, 3, 4));
+
+// Q.15
+// const array = ['a1', 'a2', 'a3', 'a4', 'a5']
+
+// var list = [];
+// array.forEach(function (arr, index) {
+//     if (index === 0) {
+//         arr = 'red';
+//     }
+//     if (index === 1) {
+//         arr = 'green';
+//     }
+//     if (index === 2) {
+//         arr = 'yello';
+//     }
+//     list.push(arr);
+// });
+
+// console.log(list);
