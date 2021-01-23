f7e01088e23d0bca750cbf3c292423bcbd116cbc
takushi <sokoninoruna@gmail.com>
2021-01-23 12:39:44 +0900

    Q16-Q20追加

---
+++ Practice/JavaScript問題集/Q16-Q20.js
@@ -0,0 +1,66 @@
+// Q.16
+// var selectedArray = array.slice(1, 4);
+
+// console.log(selectedArray);
+// console.log(array);
+
+// Q.17
+// const array = ['a1', 'a2', 'a3', 'a4', 'a5'];
+
+// var arrayStr = array.join("/")
+
+// console.log(arrayStr);
+// console.log(arrayStr.split("/"));
+
+// Q.18
+// const greet = ['おはよう', 'こんにちは', 'おやすみなさい'];
+
+// function greeting() {
+//     return greet[Math.floor(Math.random() * 3)];
+// }
+
+// console.log(greeting());
+
+// Q.19
+// nullObj = Object.create(null);
+// obj = Object.create({});
+// obj.p = 1;
+// console.log(obj);
+
+// Q.20
+// クラス
+// class Who {
+
+//     // private
+//     #email = "aaa@aaa.jp"
+//     // static
+//     static message = "hello";
+
+//     // コンストラクタ
+//     constructor(tamori) {
+//         this.name = tamori;
+//     }
+
+//     // static Method
+//     static getMessage() {
+//         return `staticMesseage:${this.message}`;
+//     }
+
+//     // getter
+//     getEmail() {
+//         return this.#email;
+//     }
+
+//     // getter
+//     // getName() {
+//     //     return this.name;
+//     // }
+// }
+
+//     Who.prototype.getName = function () {
+//     console.log(this.name);
+// }
+
+// let who = new Who("tamori");
+// who.getName();
+
