35f052b566655a4fc7fee760218e8dc088c284de
takushi <sokoninoruna@gmail.com>
2021-03-06 11:51:34 +0900

    追加

---
+++ Practice/JavaScript問題集/Q41-Q45.js
@@ -0,0 +1,48 @@
+// Q41
+// const arr = ['a', 'b', 'c', 'd', 'e'];
+
+// arr.splice(2, 0, "tamori");
+
+// console.log(arr);
+
+// // Q42
+// var o = {};
+
+// var obj = Object.create(o, Object.prototype);
+
+// if (obj === o) {
+//     console.log("onazi!!");
+// }
+
+// console.log(obj);
+
+// Q43
+//var obj = Object.create({}, { p: { value: 42 } });
+
+// var obj = Object.create({});
+// obj.p = 42;
+
+//console.log(obj.p);
+
+// Q44
+
+// var num = 1234;
+// var list = [];
+// num.toString().split('').forEach(function (value) {
+//     list.push(value);
+// });
+
+//  console.log(list);
+
+// Q45
+// [0, 1, 2, 3, 4].some(function (val, i) {
+//     if (val === 2) {
+//         // how do we stop?
+//         return true;
+//     }
+//     console.log(val);
+// });
+// 0, 1, 3, 4
+
+
+
---
+++ Practice/JavaScript練習用/NotSee/NotSee.js
@@ -0,0 +1,5 @@
+document.oncontextmenu = function () {
+    window.alert("no click!!");
+    window.location.href = "https://raise-tech.net/instructors";
+    return false;
+}
