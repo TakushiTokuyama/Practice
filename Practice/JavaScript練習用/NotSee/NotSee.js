395008dbb0bd595927112266bb9e05be9ea0a483
TakushiTokuyama <sokoninoruna@gmail.com>
2021-03-18 23:01:20 +0900

    Audio

--- Practice/JavaScript練習用/NotSee/NotSee.js
+++ Practice/JavaScript練習用/NotSee/NotSee.js
@@ -1,5 +1,21 @@
 document.oncontextmenu = function () {
     window.alert("no click!!");
+    play()
     window.location.href = "https://raise-tech.net/instructors";
     return false;
+}
+
+var music = new Audio();
+music.preload = "auto";
+music.src = "nc41828.wav";
+music.load();
+
+music.addEventListener("ended", function () {
+    music.currentTime = 0;
+    music.play();
+}, false);
+
+function play() {
+    music.loop = true;
+    music.play();
 }
