a70969ba84673d5c3dec298e8d841025d3a8680c
TakushiTokuyama <sokoninoruna@gmail.com>
2021-04-13 22:47:30 +0900

    view post処理追加

--- Node.js/Sample/SQLite/index.js
+++ Node.js/Sample/SQLite/index.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-const sqlite = require('sqlite3').verbose();
+var url = require('url');
+var html = require('fs').readFileSync('view/index.html');
 
-const fs = require('fs');
-let dbfilepath = fs.readFileSync('./settings/dbfilepath.txt', 'utf-8');
-const db = new sqlite.Database(dbfilepath);
+var db = require("./settings/dbSetting").dbConnection();
 
 var users = [];
 db.serialize(() => {
@@ -14,14 +13,21 @@ db.serialize(() => {
     });
 });
 
-var b;
-
 db.close();
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(users[0][0].Name);
+    console.log(req.url);
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(html);
+    } else if (req.url === '/post.html') {
+        var data = '';
+        req.on('data', function (chunk) { data += chunk }).on('end', function () {
+            console.log(data);
+            res.end(html);
+        })
+    }
 });
 
 
