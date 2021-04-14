34e46013f63749d8740f356d9ab79e2735d350e5
TakushiTokuyama <sokoninoruna@gmail.com>
2021-04-14 23:04:58 +0900

    ejs queryString追加 ログイン処理追加

--- Node.js/Sample/SQLite/index.js
+++ Node.js/Sample/SQLite/index.js
@@ -1,36 +1,45 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-var url = require('url');
-var html = require('fs').readFileSync('view/index.html');
+var ejs = require("ejs");
+var qs = require('querystring');
 
-var db = require("./settings/dbSetting").dbConnection();
-
-var users = [];
-db.serialize(() => {
-    db.all('SELECT * FROM User', function (err, rows) {
-        users.push(rows);
-    });
-});
-
-db.close();
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
+    var html = require('fs').readFileSync('view/index.ejs', 'utf-8');
+    var db = require("./settings/dbSetting").dbConnection();
     if (req.url === '/') {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Type', 'text/html;');
         res.end(html);
     } else if (req.url === '/post.html') {
         var data = '';
+        req.setEncoding('utf-8');
         req.on('data', function (chunk) { data += chunk }).on('end', function () {
-            console.log(data);
-            res.end(html);
+            var qsdatas = qs.parse(data);
+            console.log(qsdatas);
+
+            db.serialize(() => {
+                db.all(`SELECT * FROM User WHERE Id = ${qsdatas.id} AND Password = ${qsdatas.password}`, function (err, rows) {
+
+                    console.log(rows);
+
+                    if (rows !== undefined) {
+                        res.end(ejs.render(html, { id: rows[0]["Id"], password: rows[0]["Password"], message: "ログイン成功" }));
+                        db.close();
+                    } else {
+                        res.end(ejs.render(html, { id: "", password: "", message: "ログイン失敗" }));
+                        db.close();
+                    }
+
+                });
+            });
+
+
         })
     }
 });
-
-
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
