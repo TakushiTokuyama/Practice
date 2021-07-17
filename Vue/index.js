cbc08cf6cd8dabbe0c5fabc283712d2d42063e53
TakushiTokuyama <sokoninoruna@gmail.com>
2021-07-17 10:51:04 +0900

    add

--- Vue/index.js
+++ Vue/index.js
@@ -160,14 +160,54 @@ var v_for_object = new Vue({
 var fil = new Vue({
     el: '#fil',
     data: {
-        numbers: [ 1, 2, 3, 4, 5 ]
-      },
-      computed: {
+        numbers: [1, 2, 3, 4, 5]
+    },
+    computed: {
         evenNumbers: function () {
-          return this.numbers.filter(function (number) {
-            return number % 2 === 0
-          })
+            return this.numbers.filter(function (number) {
+                return number % 2 === 0
+            })
         }
-      }
+    }
+});
+
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+Vue.component('blog-post', {
+    props: ['post'],
+    template: '<div>'+
+                '<h3>{{post.title}}</h3>'+
+              '</div>'
+});
+
+var fil2 = new Vue({
+    el: '#fil2',
+    data: {
+        sets: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+    },
+    methods: {
+        even: function (numbers) {
+            return numbers.filter(function (number) {
+                return number % 2 === 0
+            })
+        }
+    }
+});
+
+var blog_post_demo = new Vue({
+    el: "#blog_post_demo",
+    data: {
+        posts: [
+            { id: 1, title: 'My journey'},
+            { id: 2, title: 'My Vue'}
+        ]
+    }
 });
 
